0493dfb8bf197af4cb264749c2818b9326b76a67
Francis Lagares <francis.lagares@gmail.com>
2022-06-22 20:55:13 +0200

    feat: validate user email

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
+interface SignupParams {
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+}
 @Injectable()
 export class AuthService {
-  signup() {}
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async signUp({ email }: SignupParams) {
+    const userExists = await this.prismaService.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (userExists) {
+      throw new ConflictException();
+    }
+    console.log({ userExists });
+  }
 }
